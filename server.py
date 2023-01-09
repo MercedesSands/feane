dcb9f57e2c2a3a921a3e2e6090d64f5dcd56cd41
MercedesSands <100663587+MercedesSands@users.noreply.github.com>
2023-01-09 12:55:14 -0500

    update

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, redirect
 from flask import render_template
 app = Flask(__name__)
 
@@ -6,19 +6,20 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route("/menu")
-def menu():
-    return render_template('menu.html')
+@app.route("/<menu>")
+def menu(menu):
+    return render_template(menu)
 
-@app.route("/about")
-def about():
-    return render_template('about.html')
+@app.route("/<about>")
+def about(about):
+    return render_template(about)
 
-@app.route("/book")
-def book():
-    return render_template('book.html')
+@app.route("/<book>")
+def book(book):
+    return render_template(book)
 
-                
-
+@app.route("/submit_form", methods=['POST', 'GET'])
+def submit_form():
+    return 'Got it!'
 
 
